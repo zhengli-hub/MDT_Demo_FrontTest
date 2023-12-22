6d308a93a45a580fecc6cbfb1122c53042413ca9
techming <hmeng29@wisc.edu>
2023-12-21 22:46:41 -0600

    Add geojson to the pavement map

--- js/pavement.js
+++ js/pavement.js
@@ -1,8 +1,17 @@
 // Load map for pavement sensor page
-const PaveMap = L.map("pavement-map").setView([43.063, -89.42], 13);
+const paveMap = L.map("pavement-map").setView([43.063, -89.42], 13);
 // Load OpenStreetMap
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
   maxZoom: 19,
-}).addTo(PaveMap);
+}).addTo(paveMap);
+
+fetch("../wBeltLineHW.geojson")
+  .then((response) => response.json())
+  .then((data) => {
+    L.geoJSON(data, {}).addTo(paveMap);
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON:", error);
+  });
